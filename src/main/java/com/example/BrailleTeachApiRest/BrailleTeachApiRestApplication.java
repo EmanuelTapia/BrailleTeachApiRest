7ac036edc2f14d69813d0b5488e64ebf9b14cf6d
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-06-13 19:38:28 -0500

    commit Braille Teach Api Rest

---
+++ src/main/java/com/example/BrailleTeachApiRest/BrailleTeachApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.BrailleTeachApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrailleTeachApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrailleTeachApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BrailleTeachApiRest/BrailleTeachApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrailleTeachApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrailleTeachApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
