d6bea733b11fe333ddb6893ddb13a477c2b2a751
brayan051999 <u20239953@utp.edu.pe>
2024-06-18 22:59:03 -0500

    Commit modulo

---
+++ src/main/java/com/example/BrailleTeachApiRest/entity/ModuloEntity.java
@@ -0,0 +1,26 @@
+package com.example.BrailleTeachApiRest.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "modulo")
+public class ModuloEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idModulo", columnDefinition = "bigInt(20) not null")
+    private Long idModulo;
+
+    @ManyToOne
+    @JoinColumn(name = "idAprender")
+    private AprenderEntity idAprender;
+    @ManyToOne
+    @JoinColumn(name = "idPracticar")
+    private AprenderEntity idPracticar;
+    @ManyToOne
+    @JoinColumn(name = "idMinijuego")
+    private MinijuegoEntity idMinijuego;
+}
