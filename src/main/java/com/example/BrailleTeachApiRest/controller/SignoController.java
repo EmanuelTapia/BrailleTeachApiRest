4b93235d85ff1231596471005b71bcea814a38b2
brayan051999 <u20239953@utp.edu.pe>
2024-06-27 00:42:58 -0500

    commint controller

--- src/main/java/com/example/BrailleTeachApiRest/controller/SignoController.java
+++ src/main/java/com/example/BrailleTeachApiRest/controller/SignoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/signo")
 public class SignoController {
